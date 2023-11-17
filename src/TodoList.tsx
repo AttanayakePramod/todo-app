09d7adf22de3a05fd5672d5c3ec0d9532a1f2594
Pramod <attanayakepramod@gmail.com>
2023-11-17 07:19:58 -0800

    first commit

---
+++ src/TodoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+export const TodoList = () => {
+  return (
+    <div className='container'>
+        <h1>Todo List</h1>
+        <ul>
+            <li>item 1</li>
+        </ul>
+        
+    </div>
+  )
+}
